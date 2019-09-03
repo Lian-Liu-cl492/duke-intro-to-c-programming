893ed745b16e6a483f47387b305f0c968923a404
Coursera Learner <nobody@nowhere.nul>
2019-09-03 03:07:53 +0000

    Did assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,9 +16,49 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void readResult(FILE * f){
+  char ** lines = NULL;
+  size_t num_lines = 0;
+  char * line = NULL;
+  size_t sz =0;
+  while(getline(&line, &sz, f)){
+    num_lines++;
+    lines = realloc(lines, sizeof(*lines) * num_lines);
+    lines[num_lines - 1] = line;
+    line = NULL;
+  }
+  free(line);
+
+  sortData(lines, num_lines);
+
+  for(int i=0; i<num_lines; i++){
+    printf("%s", lines[i]);
+    free(lines[i]);
+  }
+  free(lines);
+}
+
+int readFiles(char ** file_list, size_t len){
+  for(int i = 0; i<len; i++){
+    FILE* f = fopen(file_list[i], "r");
+    if(f == NULL){
+      fprintf(stderr, "Cannot open %s", file_list[i]);
+      return EXIT_FAILURE;
+    }
+    readResult(f);
+    fclose(f);
+  }
+  return EXIT_SUCCESS;
+}
+
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
-  
+  if(argc == 1){
+    readResult(stdin);
+  }
+  else{
+    return readFiles(argv+1, argc-1);
+  }  
   return EXIT_SUCCESS;
 }
