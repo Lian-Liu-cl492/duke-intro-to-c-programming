d335994356f7fe35f11d6c4aca2e30470552f141
Coursera Learner <nobody@nowhere.nul>
2019-12-28 23:12:52 +0000

    commit 1

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -6,33 +6,64 @@
 #include "outname.h"
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
-  return NULL;
+  char *line = NULL;
+  counts_t *c;
+
+  FILE* f = fopen(filename, "r");
+  if (f == NULL) {
+    perror("cannot read file");
+    exit(EXIT_FAILURE);
+  }
+  
+  c = createCounts();
+  size_t linesz = 0;
+  while (getline(&line, &linesz, f) >= 0) {
+    addCount(c, lookupValue(kvPairs, line));
+    free(line);
+    linesz = 0;
+  }
+  free(line);
+  fclose(f);
+  return c;
 }
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
+  if (argc < 3) {
+    perror("Usage: ./count_values kv list\n");
+    exit(EXIT_FAILURE);
+  }
 
- //count from 2 to argc (call the number you count i)
-
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  kvarray_t* kv = readKVs(argv[1]);
+  //count from 2 to argc (call the number you count i)
+  for(int i=2; i<argc; i++){
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
     //   (call this result c)
+    counts_t* c = countFile(argv[i], kv);
 
     //compute the output file name from argv[i] (call this outName)
-
+    char* outName = computeOutputFileName(argv[i]);
 
     //open the file named by outName (call that f)
+    FILE* f = fopen(outName, "w");
+    if (f == NULL) {
+      perror("can't open file");
+      exit(EXIT_FAILURE);
+    }
 
     //print the counts from c into the FILE f
-
+    printCounts(c, f);
+    
     //close f
+    fclose(f);
 
     //free the memory for outName and c
+    free(outName);
+    freeCounts(c);
+  }
 
-
-
- //free the memory for kv
-
+  //free the memory for kv
+  freeKVs(kv);
   return EXIT_SUCCESS;
 }
