a2485e563b56e4a425cbd7006b2bbc2708b1fc16
Coursera Learner <nobody@nowhere.nul>
2020-01-03 03:37:08 +0000

    a

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -7,7 +7,6 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
   hand->n_cards = 0;
   const char * p = str;
   while (*p != '\n' && *(p+1) != '\n' && *(p+2) != '\n'){
-    p += 1;
     if(*p == '?'){
       add_empty_card(hand);
       add_future_card(fc, atoi(&*(p+1)), hand->cards[hand->n_cards - 1]);
