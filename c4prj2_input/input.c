e9bb43c66e6d7e2e4c9625d0f0d6b16338fbce7a
Coursera Learner <nobody@nowhere.nul>
2020-01-03 02:08:54 +0000

    part2

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -4,11 +4,13 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
   deck_t * hand = malloc(sizeof(*hand));
   hand->cards = NULL;
   hand->n_cards = 0;
-  char * curr = str;
-  while(*curr != '\n'){
+  while(*str != '\n'){
+    card_t c = card_from_letters(*str, *(str+1));
     hand->cards = realloc(hand->cards, sizeof(*(hand->cards)) * ++hand->n_cards);
-    hand->cards[hand->n_cards - 1] = card_from_letters(*curr, *(curr+1));
-    curr += 3;
+    hand->cards[hand->n_cards - 1] = malloc(*(hand->cards[hand->n_cards - 1]));
+    hand->cards[hand->n_cards - 1]->value = c.value();
+    hand->cards[hand->n_cards - 1]->suit = c.suit(); 
+    str += 3;
   }
   if(hand->n_cards < 5){
     perror("Number of cards is less than five!");
@@ -26,7 +28,7 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
   while (getline(&line, &linesz, f) >= 0) {
     deck_t * hand = hand_from_string(line, fc);
     hands = realloc(hands, sizeof(*hands) * ++(*n_hands));
-    hands[n_hands - 1] = hand;
+    hands[(int)n_hands - 1] = hand;
     free(line);
     linesz = 0;
   }
