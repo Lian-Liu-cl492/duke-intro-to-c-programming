2377a637f728e7ac21fe33283513f848626ced6a
Coursera Learner <nobody@nowhere.nul>
2020-01-03 03:40:22 +0000

    a

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -13,10 +13,10 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
 }
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
-  if(deck->n_cards < fc->n_decks){
-    perror("The deck does not have enough cards!");
-    exit(EXIT_FAILURE);
-  }
+//  if(deck->n_cards < fc->n_decks){
+//    perror("The deck does not have enough cards!");
+//    exit(EXIT_FAILURE);
+//  }
   for(int i=0; i<fc->n_decks; i++){
     for(int j=0; j<fc->decks[i].n_cards; j++){
       fc->decks[i].cards[j]->value = deck->cards[i]->value;
