eff07fcfbd94bad40b43cc81e76f5a69937563db
Coursera Learner <nobody@nowhere.nul>
2020-01-03 06:07:16 +0000

    part2

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -5,22 +5,49 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
   deck_t * hand = malloc(sizeof(*hand));
   hand->cards = NULL;
   hand->n_cards = 0;
+  char delim[] = " ";
+  char * p = strdup(str);
+  if (strlen(p) > 0){
+    p[strlen(p) - 1] = '\0';
+  }
+  char *ptr = strtok(p, delim);
+  while(ptr != NULL)
+  {
+    if(ptr[0] == '?'){
+      add_empty_card(hand);
+      char * s = strndup(ptr+1, strlen(ptr)-1);
+      add_future_card(fc, atoi(s), hand->cards[hand->n_cards - 1]);
+      free(s);
+    } else {
+      card_t c = card_from_letters(ptr[0], ptr[1]);
+      add_card_to(hand, c);
+    }
+    ptr = strtok(NULL, delim);
+  }
+  free(p);
+
+/*
   const char * p1 = str;
   const char * p2 = strchr(str, ' ');
-  while(p1 != NULL){
+  while(p1 != '\0'){
     fprintf(stderr, "p1 = %s\n", p1);
     if(*p1 == '?'){
       add_empty_card(hand);
-      char * s = strndup(p1+1, p2-p1-2);
+      char * s = strndup(p1+1, p2-p1-1);
+      fprintf(stderr, "s = %s\n", s);
       add_future_card(fc, atoi(s), hand->cards[hand->n_cards - 1]);
       free(s);
     } else {
       card_t c = card_from_letters(*p1, *(p1+1));
       add_card_to(hand, c);
     }
-    p1 = (p2 == NULL ) ? p2 : p2+1;
-    p2 = (p2 == NULL) ? p2 : strchr(p2+1, ' ');
+    p1 = p2+1;
+    p2 = strchr(p2+1, ' ');
+    if(p2 == NULL){
+      p2 = strchr(p1, '\n');
+    }
   }
+*/
   if(hand->n_cards < 5){
     perror("Number of cards is less than five!");
     exit(EXIT_FAILURE);
